2ce1bcf59ca592e0718e28b87276612592aa4e6c
donchi fofack donald <donchidonald2000@gmail.com>
2024-01-31 22:01:28 +0100

    upate

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,13 +6,17 @@ import { HousingLocation } from '../housinglocation';
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <h2 class="listing-heading">{{ housingLocation.name  }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <p>{{today | date: 'medium' }}</p>
+      <p>{{ money | currency : 'FCFA'}}</p>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
+  today = new Date();
+  money = 1000;
 
 }
